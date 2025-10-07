07ca515a61192f55dac40e35b82af61463b7138a
Thomas Ahn <166737116+quakeq@users.noreply.github.com>
2025-10-07 11:59:01 -0700

    removed prototype classes from DEV, branches with prototype should have prototype, any others should not be prototype

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ICBPL_robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ICBPL_robot.java
@@ -7,9 +7,7 @@ import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.subsystems.intake.IntakeProto;
 import org.firstinspires.ftc.teamcode.util.Logger;
-import org.firstinspires.ftc.teamcode.subsystems.flywheel.FlywheelProto;
 
 public class ICBPL_robot extends Robot {
 
@@ -17,57 +15,18 @@ public class ICBPL_robot extends Robot {
 
     private final Logger logger;
 
-    private final FlywheelProto flywheelProto;
-    private final IntakeProto intakeProto;
-
     public enum OpModeType{
         MainOP,
         AUTO,
-        INTAKEPROTO
     }
 
-    public ICBPL_robot(OpModeType mode, HardwareMap hMap, Telemetry log){
-        if (mode == OpModeType.MainOP) initMainOp();
-        else if (mode == OpModeType.AUTO) initAuto();
-        else if (mode == OpModeType.INTAKEPROTO) initIntakeProto();
-
-
+    public ICBPL_robot(OpModeType mode, HardwareMap hMap, Telemetry log) {
         logger = new Logger(log);
-        flywheelProto = new FlywheelProto(hMap, logger);
-        intakeProto = new IntakeProto(hMap, logger);
-    }
-
-    private void initAuto(){
-        this.schedule();
-        this.register(flywheelProto, logger);
-    }
-
-    private void initMainOp(){
-        this.schedule();
-        this.register(flywheelProto, logger);
-    }
-
-    private void initIntakeProto(){
-        this.schedule();
-        this.register(intakeProto, logger);
     }
 
 
-    /***Flywheel Commands***/
-    public InstantCommand spinFlywheel(){
-        return new InstantCommand(flywheelProto::spinWheel, flywheelProto);
-    }
-    public InstantCommand stopFlywheel(){
-        return new InstantCommand(flywheelProto::stopWheel, flywheelProto);
-    }
 
-    /***Intake Commands***/
-    public InstantCommand spinIntake(){
-        return new InstantCommand(intakeProto::spin, intakeProto);
-    }
-    public InstantCommand stopIntake(){
-        return new InstantCommand(intakeProto::stop, intakeProto);
-    }
+
 
 
 }
