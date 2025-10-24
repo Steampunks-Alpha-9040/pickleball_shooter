0c63be7a1eeada55bbc208f867c3a4459618f247
Thomas Ahn <166737116+quakeq@users.noreply.github.com>
2025-10-23 21:51:49 -0700

    deleted extranenous files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/MainOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/MainOp.java
@@ -1,52 +1,20 @@
 package org.firstinspires.ftc.teamcode.programs;
 
 
-import com.bylazar.telemetry.PanelsTelemetry;
-import com.bylazar.utils.LoopTimer;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-
-import org.firstinspires.ftc.teamcode.subsystems.Feeder;
-import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
-import org.firstinspires.ftc.teamcode.subsystems.LoggingPanels;
-import org.firstinspires.ftc.teamcode.util.Drawer;
-
-import dev.frozenmilk.mercurial.commands.groups.Parallel;
 
 
 @TeleOp(name = "Main_PickleTeleOp")
 public class MainOp extends BaseOpMode {
 
-    private final PanelsTelemetry panelsManager = LoggingPanels.getPanelsManager();
-
-    private final LoopTimer loopTimer = new LoopTimer();
-
-    private final Drawer field = new Drawer();
-
-    private final Parallel runOuttake = new Parallel(
-        Feeder.getInstance().startFeeder()
-    );
 
     @Override
     public void init() {
-        panelsManager.getTelemetry().debug("hi world");
-        panelsManager.getTelemetry().update(telemetry);
+
     }
 
     @Override
     public void loop() {
-        loopTimer.start();
-        updateLogging();
 
-
-        panelsManager.getTelemetry().addData("LoopTime", panelsManager.getTelemetry().getTimeSinceLastUpdate());
-
-
-
-        loopTimer.end();
-    }
-
-    public void updateLogging(){
-        Flywheel.getInstance().logFlywheel();
     }
 }
