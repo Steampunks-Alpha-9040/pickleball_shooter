0ea8a7abba2113a54ec3c6544d277dbb83b10e64
Thomas Ahn <166737116+quakeq@users.noreply.github.com>
2025-10-28 23:42:01 -0700

    need to finish flywheel to fit nextFTC

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/BaseOpMode.java
@@ -4,9 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
 
+import dev.nextftc.ftc.NextFTCOpMode;
 
 
-
-@Flywheel.Attach
-public abstract class BaseOpMode extends OpMode {
+public abstract class BaseOpMode extends NextFTCOpMode {
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flywheel.java
@@ -15,7 +15,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.feedback.FeedbackType;
+import dev.nextftc.control.feedback.PIDCoefficients;
+import dev.nextftc.control.feedback.PIDElement;
+import dev.nextftc.control.feedforward.BasicFeedforward;
+import dev.nextftc.control.filters.FilterElement;
+import dev.nextftc.control.interpolators.ConstantInterpolator;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
+import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.hardware.impl.CRServoEx;
 import dev.nextftc.hardware.impl.MotorEx;
 
@@ -24,7 +35,40 @@ public class Flywheel implements Subsystem {
 
     public static final Flywheel INSTANCE = new Flywheel();
 
-    private final MotorEx flywheel = new MotorEx(Constants.FlywheelConstants.flywheelName).brakeMode().zeroed();
+    private MotorEx flywheel;
+
+    private ControlSystem calculator = new ControlSystem(
+            new PIDElement(
+                    FeedbackType.VELOCITY,
+                    Constants.FlywheelConstants.kP,
+                    Constants.FlywheelConstants.kI,
+                    Constants.FlywheelConstants.kD
+                    ),
+            new BasicFeedforward(
+                    Constants.FlywheelConstants.kF
+            ),
+            new FilterElement(),
+            new ConstantInterpolator()
+
+    )
+
+    private Flywheel(){}
+
+    @Override
+    public void initialize(){
+        flywheel = new MotorEx(Constants.FlywheelConstants.flywheelName).brakeMode().zeroed();
+    }
+
+    public Command spinFlywheel(){
+        return new LambdaCommand("spinFlywheel")
+                .setStart(() -> flywheel.setPower(1.0))
+                .setIsDone(() -> Gamepads.gamepad1().a().get());
+    }
+
+    public void powerFlywheel(double power){
+        flywheel.setPower(power);
+    }
+
 
 
 
