5612d7fe3a4b37612de36a1bcc10589a13291711
Thomas Ahn <166737116+quakeq@users.noreply.github.com>
2025-10-31 18:01:30 -0700

    works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/BaseOpMode.java
@@ -2,12 +2,10 @@ package org.firstinspires.ftc.teamcode.programs;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.teamcode.subsystems.Drivebase;
 
 import dev.nextftc.ftc.NextFTCOpMode;
 
 
 public abstract class BaseOpMode extends NextFTCOpMode {
-     protected Drivebase drivebase = Drivebase.INSTANCE;
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/MainOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/MainOp.java
@@ -1,44 +1,35 @@
 package org.firstinspires.ftc.teamcode.programs;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.sun.tools.javac.Main;
 
-import org.firstinspires.ftc.teamcode.subsystems.Drivebase;
 
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.components.BulkReadComponent;
 import dev.nextftc.hardware.driving.MecanumDriverControlled;
+import dev.nextftc.hardware.impl.ServoEx;
 
 
 @TeleOp(name = "Main_PickleTeleOp")
-public class MainOp extends BaseOpMode {
-
-    private final Command drive = super.drivebase.getRobotMecanumDriver();
-
-    public MainOp(){
-        addComponents(
-                new SubsystemComponent(
-                        super.drivebase
-                ),
-                BulkReadComponent.INSTANCE,
-                BindingsComponent.INSTANCE
-        );
-    }
+public class MainOp extends LinearOpMode {
 
 
 
 
     @Override
-    public void onStartButtonPressed() {
-        drive.schedule();
-
-    }
-
-    @Override
-    public void onUpdate() {
+    public void runOpMode() throws InterruptedException {
+        Servo servo = hardwareMap.get(Servo.class, "servo");
+        servo.setPosition(0.0);
+        servo.setDirection(Servo.Direction.FORWARD);
+        waitForStart();
+        while (opModeIsActive()){
+            servo.setPosition(0.0);
+        }
 
     }
 }
