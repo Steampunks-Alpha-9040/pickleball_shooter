9484861b9ce55493377d1209f143fe43003eb225
Thomas Ahn <166737116+quakeq@users.noreply.github.com>
2025-10-31 23:04:55 -0700

    added dv stufff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivebase.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import androidx.annotation.NonNull;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.teamcode.Constants;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.hardware.driving.FieldCentric;
+import dev.nextftc.hardware.driving.HolonomicMode;
+import dev.nextftc.hardware.driving.MecanumDriverControlled;
+import dev.nextftc.hardware.impl.Direction;
+import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.impl.IMUEx;
+
+
+public class Drivebase implements Subsystem {
+
+    public static final Drivebase INSTANCE = new Drivebase();
+    private Drivebase() { }
+    public static Follower follower;
+
+    private MotorEx FL = new MotorEx(Constants.DrivebaseConstants.FL).brakeMode().reversed();
+    private MotorEx FR = new MotorEx(Constants.DrivebaseConstants.FR).brakeMode();
+    private MotorEx BL = new MotorEx(Constants.DrivebaseConstants.BL).brakeMode().reversed();
+    private MotorEx BR = new MotorEx(Constants.DrivebaseConstants.BR).brakeMode();
+    private IMUEx imu = new IMUEx(Constants.DrivebaseConstants.IMU, Direction.DOWN, Direction.FORWARD).zeroed();
+
+
+    public MecanumDriverControlled getMecanumDriver(){
+        return new MecanumDriverControlled(
+            FL, FR, BL, BR,
+            Gamepads.gamepad1().leftStickY().negate(),
+            Gamepads.gamepad1().leftStickX(),
+            Gamepads.gamepad1().rightStickX(),
+            new FieldCentric(imu)
+        );
+    }
+}
