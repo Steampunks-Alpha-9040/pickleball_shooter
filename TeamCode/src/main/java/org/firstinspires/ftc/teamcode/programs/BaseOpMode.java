073b616472eab8a7c06524f785b9681eb031f7d6
Thomas Ahn <166737116+quakeq@users.noreply.github.com>
2026-01-01 18:49:45 -0800

    lookup wroks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/BaseOpMode.java
@@ -6,12 +6,8 @@ import com.bylazar.telemetry.PanelsTelemetry;
 
 
 import org.firstinspires.ftc.teamcode.subsystems.Drivebase;
-import org.firstinspires.ftc.teamcode.subsystems.Feeder;
 import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
-import org.firstinspires.ftc.teamcode.subsystems.Indexer;
-import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Turret;
-import org.firstinspires.ftc.teamcode.subsystems.Vision;
 
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
@@ -25,22 +21,14 @@ public abstract class BaseOpMode extends NextFTCOpMode {
     protected PanelsField field = PanelsField.INSTANCE;
     protected Flywheel flywheel = Flywheel.INSTANCE;
     protected Drivebase drivebase = Drivebase.INSTANCE;
-    protected Indexer indexer = Indexer.INSTANCE;
-    protected Feeder feeder = Feeder.INSTANCE;
-    protected Intake intake = Intake.INSTANCE;
     protected Turret turret = Turret.INSTANCE;
-    protected Vision vision = Vision.INSTANCE;
 
     public BaseOpMode(){
         addComponents(
                 new SubsystemComponent(
                         drivebase,
-                        indexer,
-                        feeder,
                         flywheel,
-                        intake,
-                        turret,
-                        vision
+                        turret
                 ),
                 BulkReadComponent.INSTANCE,
                 BindingsComponent.INSTANCE
