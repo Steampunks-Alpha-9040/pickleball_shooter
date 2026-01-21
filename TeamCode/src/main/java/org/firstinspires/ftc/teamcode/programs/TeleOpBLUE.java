bd3a1156e447f088c2f4e7a2bc08f419f0623480
OkSureWhyNot <tbaghalian@gmail.com>
2026-01-20 22:56:31 -0800

    tuning vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/TeleOpBLUE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/programs/TeleOpBLUE.java
@@ -32,7 +32,7 @@ public class TeleOpBLUE extends BaseOpMode {
 
         drivebase.setFollower(PedroComponent.follower());
         // Auto ends on 63.307, 7.811, 90
-        drivebase.getFollower().setStartingPose(new Pose(72-Constants.robotWidth/2, Constants.robotLength/2, Math.toRadians(90)));
+        drivebase.getFollower().setStartingPose(new Pose(72, 72, Math.PI/2));
         drivebase.INSTANCE.getBR().setCurrentPosition(0);
         drivebase.INSTANCE.getFL().setCurrentPosition(0);
 
