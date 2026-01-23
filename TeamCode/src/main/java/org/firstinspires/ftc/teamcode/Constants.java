ddc5445e97c68fa19a7df4c85cb4b78e0addc150
OkSureWhyNot <tbaghalian@gmail.com>
2026-01-22 20:42:05 -0800

    tuning values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -110,10 +110,10 @@ public class Constants {
         public static final String rampLeftSensor = "rampLeftSensor";
         public static final double motorToIndexer = 0.0;
 
-        public static final double indexer_kP = 0.007; //The like main pushing force, the constant :)
+        public static final double indexer_kP = 0.001; //The like main pushing force, the constant :)
         public static final double indexer_kI = 0.0; //The more you aren't making it to the setpoint, the more you increase this
-        public static final double indexer_kD = 0.0003; //slows down/speeds up the closer/farther away u are
-        public static final double indexer_kF = 0.05; //friction, tune so when the turret barely moves
+        public static final double indexer_kD = 0.000; //slows down/speeds up the closer/farther away u are
+        public static final double indexer_kF = 0.07; //friction, tune so when the turret barely moves
 
         public static final double indexer_DutyCycle = 0.4;
 
